9c64387a6ebdd9d9cc72d4bdb44e02bd45025ff8
Jishllg Laptop <jishllg@gmail.com>
2020-10-22 17:01:18 -0500

    Added ID and delete functionality.

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var app = express();
 const port = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "public/")));
 app.listen(port, function() {
     console.log("App listening on PORT: " + port);
 });
@@ -20,30 +21,32 @@ app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "public/notes.html"));
 });
 
+// Sending notes from database file
 app.get("/api/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "db/db.json"));
-    // fs.readFile(path.join(__dirname, "db/db.json"), 'utf8', function(error, data) {
-    //     if (error) throw error;
-    //     res.sendFile(data);
-    // });
 });
 
+// Saving new note to database file
 app.post("/api/notes", function(req, res) {
     fs.readFile(path.join(__dirname, "db/db.json"), 'utf8', function(err, data) {
         let notes = JSON.parse(data);
         let newNote = req.body;
-        newNote.id = notes.length() + 1;
+        newNote.id = notes.length + 1;
         notes.push(newNote);
-        fs.writeFile(path.join(__dirname, "db/db.json"), JSON.stringify(notes));
+        fs.writeFile(path.join(__dirname, "db/db.json"), JSON.stringify(notes), 'utf8', () => {
+            res.json(true)
+        });
     });
 });
 
-// app.post("/api/notes/:id", function(req, res) {
-//     fs.readFile(path.join(__dirname, "db/db.json"), 'utf8', function(err, data) {
-//         let notes = JSON.parse(data);
-//         let newNote = req.body;
-//         newNote.id = notes.length() + 1;
-//         notes.push(newNote);
-//         fs.writeFile(path.join(__dirname, "db/db.json"), JSON.stringify(notes));
-//     });
-// });
+// Deleting note from database file based on id
+app.delete("/api/notes/:id", function(req, res) {
+    fs.readFile(path.join(__dirname, "db/db.json"), 'utf8', function(err, data) {
+        let notes = JSON.parse(data);
+        notes.splice(req.params.id - 1, 1);
+        notes.forEach((note, index) => {note.id = index;});
+        fs.writeFile(path.join(__dirname, "db/db.json"), JSON.stringify(notes), 'utf8', () => {
+            res.json(true)
+        });
+    });
+});
